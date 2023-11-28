dcff23efb193ca7b2e148f956e41df460a76f478
Cody Ko In A Box <codykoinabox@gmail.com>
2023-11-28 20:23:29 -0300

    criar consultas 2 e 3

--- consultas.sql
+++ consultas.sql
@@ -6,3 +6,13 @@
 SELECT chassi, placa, marca, modelo, cor FROM veiculo v
 INNER JOIN precisa_manutencao pm
 ON v.chassi = pm.veiculo_chassi;
+
+--@block | 2-> Verificar quantos carros antigos (antes de 2010) estao alugados
+SELECT chassi, placa, marca, modelo, ano FROM veiculo v
+WHERE v.alugado = true
+AND v.ano < 2010;
+
+--@block | 3-> Verificar quantos carros novos (depois de 2010) estao alugados
+SELECT chassi, placa, marca, modelo, ano FROM veiculo v
+WHERE v.alugado = true
+AND v.ano > 2010;
