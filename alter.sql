1276e0c421948543f996314304449caadffac354
Cody Ko In A Box <codykoinabox@gmail.com>
2023-11-28 20:55:55 -0300

    criar enunciado das 4 atualizacoes de tabela

---
+++ alter.sql
@@ -0,0 +1,9 @@
+-- Baseado nas atualizacoes de tabela: https://www.invertexto.com/?n=fXc8giI
+
+--@block | 1-> Aumenta o tamanho do varchar da placa do veiculo (para caso as placas fiquem maiores no futuro)
+
+--@block | 2-> Apaga o campo cor dos veiculos (caso nao precisa mais saber a cor)
+
+--@block | 3-> Insere campo do tipo BOOL no veiculo que define se o veiculo e eletrico (caso a empresa decida comecar a comprar carros eletricos)
+
+--@block | 4-> Insere campo de idade no cliente
