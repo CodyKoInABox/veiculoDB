a125bb7cd1c2107a9216c0c5f7289a8ea9c0f82f
Cody Ko In A Box <codykoinabox@gmail.com>
2023-11-28 21:04:00 -0300

    criar as 4 atualizacoes de tabela

--- alter.sql
+++ alter.sql
@@ -1,9 +1,17 @@
 -- Baseado nas atualizacoes de tabela: https://www.invertexto.com/?n=fXc8giI
 
 --@block | 1-> Aumenta o tamanho do varchar da placa do veiculo (para caso as placas fiquem maiores no futuro)
+ALTER TABLE veiculo
+MODIFY COLUMN placa VARCHAR(8);
 
 --@block | 2-> Apaga o campo cor dos veiculos (caso nao precisa mais saber a cor)
+ALTER TABLE veiculo
+DROP COLUMN cor;
 
 --@block | 3-> Insere campo do tipo BOOL no veiculo que define se o veiculo e eletrico (caso a empresa decida comecar a comprar carros eletricos)
+ALTER TABLE veiculo
+ADD eletrico BOOLEAN;
 
 --@block | 4-> Insere campo de idade no cliente
+ALTER TABLE cliente
+ADD idade INT;
