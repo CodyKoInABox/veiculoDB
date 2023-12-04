238edddaba0a344c98a662d3a6c475abc906709d
Cody Ko In A Box <codykoinabox@gmail.com>
2023-12-04 19:52:20 -0300

    criar trigger 4 e 5

--- triggers.sql
+++ triggers.sql
@@ -11,5 +11,23 @@ SET data_prevista = CURDATE() + NEW.dias;
 --@block | 3-> Quando uma devolucao e criada, o trigger verifica se o dia da devolucao tem atraso em relacao a data prevista, se tiver, cria uma multa e adiciona ao valor real (multa = dobro da diaria por dia atrasado
 
 --@block | 4-> Quando um novo aluguel e criado, o trigger muda o bool do veiculo alugado para true
+CREATE TRIGGER novaDevolucao BEFORE UPDATE ON aluguel
+FOR EACH ROW
+BEGIN
+    IF !(NEW.retirada_status_id <=> OLD.retirada_status_id) THEN
+        UPDATE veiculo v
+        SET v.alugado = true
+        WHERE v.chassi = aluguel.veiculo_chassi
+    END IF;
+END;
 
 --@block | 5-> Quando uma devolucao e criada, o trigger muda o bool do veiculo devolvido para false
+CREATE TRIGGER novaDevolucao BEFORE UPDATE ON aluguel
+FOR EACH ROW
+BEGIN
+    IF !(NEW.devolucao_status_id <=> OLD.devolucao_status_id) THEN
+        UPDATE veiculo v
+        SET v.alugado = false
+        WHERE v.chassi = aluguel.veiculo_chassi
+    END IF;
+END;
